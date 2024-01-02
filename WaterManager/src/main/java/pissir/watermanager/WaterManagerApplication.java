01c084e81873ba30205f8bf0a253a5f73b8be4e9
alessandrogattico <gattico.alessandro@gmail.com>
2024-01-02 21:31:41 +0100

    aggiunta progetto 2

---
+++ WaterManager/src/main/java/pissir/watermanager/WaterManagerApplication.java
@@ -0,0 +1,13 @@
+package pissir.watermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterManagerApplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run(WaterManagerApplication.class, args);
+	}
+	
+}
---
+++ WaterManager/src/test/java/pissir/watermanager/WaterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pissir.watermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterManagerApplicationTests {
+	
+	@Test
+	void contextLoads () {
+	}
+	
+}
